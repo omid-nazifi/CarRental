07a42baf46c3f5133061af63b2335d78653e5497
omid-nazifi <omid.nazifi@gmail.com>
2021-04-09 23:13:17 +0200

    create Mock data for testing

--- CarRental/src/main/java/at/wiencampus/se/carrental/controller/CustomerController.java
+++ CarRental/src/main/java/at/wiencampus/se/carrental/controller/CustomerController.java
@@ -5,24 +5,39 @@ import at.wiencampus.se.carrental.dal.Customer;
 import at.wiencampus.se.carrental.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/customer")
-public class CustomerController {
+public class CustomerController extends IController {
 
     @Autowired
     CustomerService customerService;
 
-    @GetMapping
-    public ResponseEntity<List<Customer>> getAllCustomers()
-    {
-        List<Customer> customerList=customerService.getCustomers();
+    @GetMapping(value = "/all")
+    public ResponseEntity<List<Customer>> getAllCustomers() {
+        List<Customer> customerList = customerService.getCustomers();
         return new ResponseEntity<>(customerList, HttpStatus.OK);
+    }
+
+    @PostMapping(value = "/signup", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity registerNewCustomer(@RequestBody Customer customer) {
+        if (customer == null) {
+            return new ResponseEntity<String>("Customer object is null", HttpStatus.NOT_ACCEPTABLE);
+        }
+        customer.setCustomerId(123L);
+        return new ResponseEntity(customer, HttpStatus.OK);
+    }
+
+    @PostMapping(value = "/signin", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity login(@RequestBody Customer customer) {
+        if (customer == null) {
+            return new ResponseEntity<String>("Customer object is null", HttpStatus.NOT_ACCEPTABLE);
+        }
+        return new ResponseEntity("you login successfully!", HttpStatus.OK);
     }
 }
