5d22d831544cd41b6de5e357934033f2e09d3efd
faelfael <fahmi.elgh@gmail.com>
2021-04-11 23:24:48 +0200

    adding Dockerfile and enable cross origin

--- CarRental/src/main/java/at/wiencampus/se/carrental/controller/VehicleController.java
+++ CarRental/src/main/java/at/wiencampus/se/carrental/controller/VehicleController.java
@@ -8,12 +8,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.Collections;
-import java.util.Date;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping("/vehicle")
 public class VehicleController extends IController {
 
