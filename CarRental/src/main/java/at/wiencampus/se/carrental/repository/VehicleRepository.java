0703dd9fe8eb4c4fc65daa4da5da3dfd66e16a77
omid-nazifi <omid.nazifi@gmail.com>
2021-05-10 00:32:44 +0200

    remove previous carrental src

--- CarRental/src/main/java/at/wiencampus/se/carrental/repository/VehicleRepository.java
+++
@@ -1,9 +0,0 @@
-package at.wiencampus.se.carrental.repository;
-
-import at.wiencampus.se.carrental.dal.Vehicle;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
-
-@EnableMongoRepositories
-public interface VehicleRepository extends MongoRepository<Vehicle, String> {
-}
