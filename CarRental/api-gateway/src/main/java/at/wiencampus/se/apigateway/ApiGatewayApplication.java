bea6c7000e814adabe42e4484b9684162deb355e
Navid <navid.nazifi@gmail.com>
2021-05-09 08:53:20 +0200

    init car rental microservices

---
+++ CarRental/api-gateway/src/main/java/at/wiencampus/se/apigateway/ApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package at.wiencampus.se.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+
+}
---
+++ CarRental/customer-service/src/main/java/at/wiencampus/se/customerservice/CustomerServiceApplication.java
@@ -0,0 +1,13 @@
+package at.wiencampus.se.customerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CustomerServiceApplication.class, args);
+    }
+
+}
