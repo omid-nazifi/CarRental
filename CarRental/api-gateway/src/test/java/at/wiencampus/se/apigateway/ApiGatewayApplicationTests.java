3dad57b45c40fa9c99ba92657e6dba3e3133b43e
Navid <navid.nazifi@gmail.com>
2021-05-09 08:53:20 +0200

    init car rental microservices

---
+++ CarRental/api-gateway/src/test/java/at/wiencampus/se/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package at.wiencampus.se.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
